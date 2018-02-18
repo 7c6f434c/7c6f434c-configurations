bd791e3f2aa4a99e1f249a11af3ae30e0554fdb2
Michael Raskin <7c6f434c@mail.ru>
2018-02-18 14:37:09 +0100

    Syncing from Monotone at 20180218-143708. Last commit message was Trying to build a laptop-bootable image instead of the VM

---
+++ lesser-expressions/raskin-user-light.nix
@@ -0,0 +1,48 @@
+let
+NIXPKGS_env = builtins.getEnv "NIXPKGS";
+pkgsPath = if NIXPKGS_env == "" then <nixpkgs> else NIXPKGS_env;
+pkgs = import pkgsPath {};
+allOutputNames = packages: builtins.attrNames
+      (pkgs.lib.fold
+        (a: b: b //
+          (builtins.listToAttrs (map (x: {name = x; value = x;}) a.outputs or ["out"])))
+        {} packages);
+fullEnv = name: packages:
+  pkgs.buildEnv {
+      name = name;
+      paths = packages;
+      ignoreCollisions = false;
+      checkCollisionContents = true;
+      pathsToLink = ["/"];
+      extraOutputsToInstall = (allOutputNames packages);
+    };
+in with pkgs;
+
+fullEnv "main-light-package-set"
+      [
+        squid git monotone fbida fbterm postgresql95 expect pmount fdm python2
+        slmenu mcabber ii irssi elinks androidenv.androidsdk_4_2 rsync
+        pv lvm2 mariadb remind xterm zsh mlterm ntp mc ncdu
+        htop iotop powertop mtr bind inotify-tools xorg.setxkbmap xfig
+        firefox vimHugeX evince xpdf zathura mplayer alsaUtils xvfb_run
+        xorg.xmodmap bc xdotool lftp wget unzip p7zip gnumake xcape
+        xsel xclip pulseaudioLight ripmime xscreensaver xorg.xsetroot lsof rofi
+        fpc nix-repl graphviz diffutils fontconfig compton xorg.xprop xorg.xwininfo
+        xorg.xlsclients fortune fuse3
+        xdaliclock openvpn iftop
+        (import ./texlive-set.nix pkgs)
+        (pkgs.lib.overrideDerivation
+           pkgs.slimerjs (x: {
+             buildPhase = ''
+               (
+                  mkdir omni.ja.unpacked
+                  cd omni.ja.unpacked
+                  unzip ../omni.ja
+                  patch -Np0 -i ${/home/repos/slimerjs-omni-ja-overrides/overrides.patch}
+                  zip ../omni.ja -r . 
+                  cd ..
+                  test -d chrome && cp -r omni.ja.unpacked/* .
+               )
+             '';
+             }))
+      ]      
---
+++ lesser-expressions/texlive-set.nix
@@ -0,0 +1,30 @@
+pkgs:
+/*
+let 
+  texLivePaths = with pkgs; [
+          texLive texLiveExtra lmodern texLiveCMSuper 
+	  texLiveLatexXColor texLivePGF
+	  texLiveBeamer texLiveModerncv tipa texLiveContext 
+          texDisser lmmath texinfo5 tex4ht texLiveModerntimeline
+     ];
+  myTexLive = pkgs.texLiveAggregationFun {
+    paths = texLivePaths;
+  };
+in
+  myTexLive
+*/
+
+pkgs.texlive.combine {
+  inherit(pkgs.texlive)
+    scheme-medium collection-langcyrillic 
+    cm-super cm-unicode xcolor pgf beamer moderncv tipa disser urlbst
+    moderntimeline texinfo metafont multibib collection-fontutils
+    epstopdf epspdfconversion epspdf algorithm2e relsize type1cm logreq
+    ucs todonotes xargs forloop pbox varwidth bigfoot environ trimspaces
+    forest arydshln pgfopts elocalloc libertine mweights fontaxes
+    totpages comment soul multirow threeparttable lastpage preprint
+    mnsymbol paralist animate media9 ocgx2 breakcites biblatex bibtex
+    xstring wrapfig ifoddpage inlinedef draftwatermark everypage
+    enumitem cutwin
+    ;
+}
