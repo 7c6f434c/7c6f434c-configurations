8b31d3686e96f417a9b7f07c32219ceae2719f11
Michael Raskin <7c6f434c@mail.ru>
2018-04-27 02:45:50 +0200

    Syncing from Monotone at 20180427-024550. Last commit message was more stuff

--- lesser-expressions/raskin-user-light.nix
+++ lesser-expressions/raskin-user-light.nix
@@ -29,7 +29,7 @@ fullEnv "main-light-package-set"
         xsel xclip pulseaudioLight ripmime xscreensaver xorg.xsetroot lsof rofi
         fpc nix-repl graphviz diffutils fontconfig compton xorg.xprop xorg.xwininfo 
         xorg.xlsclients fortune fuse3 openssl axel arping whois hping
-        xdaliclock openvpn iftop file patchutils zip gawk
+        xdaliclock openvpn iftop file patchutils zip gawk parallel perl
         man manpages
         (import ./texlive-set.nix pkgs)
       ]      
