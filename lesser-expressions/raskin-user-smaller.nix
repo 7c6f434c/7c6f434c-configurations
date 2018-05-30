d03fe6bd0a2ad6df7337eef1126906cda4aa1649
Michael Raskin <7c6f434c@mail.ru>
2018-05-30 02:46:38 +0200

    Syncing from Monotone at 20180530-024638. Last commit message was More stuff

--- lesser-expressions/raskin-user-smaller.nix
+++ lesser-expressions/raskin-user-smaller.nix
@@ -35,7 +35,7 @@ linkFarm "raskin-packages" ([
 		(map justUse [
 		"gsettings_desktop_schemas" "gtk3" "weechat-matrix-bridge"
 		"fuse" "mysql" "openssl" "opencv" "postgresql" "sqlite"
-		"icedtea_web" "love_0_10" "love_0_9" "libpulseaudio"
+		"icedtea_web" "love_0_10" "love_0_9" "love_11" "libpulseaudio"
                 "xfig" "transfig" "wgetpaste" "gdmap" "netcat" "python3"
 		])
     ++
