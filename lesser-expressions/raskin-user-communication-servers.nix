22244a4531a3955c39f8a47805a35182a0a30575
Michael Raskin <7c6f434c@mail.ru>
2018-08-14 02:46:23 +0300

    Syncing from Monotone at 20180814-024623. Last commit message was Trying to precreate more

---
+++ lesser-expressions/raskin-user-communication-servers.nix
@@ -0,0 +1,28 @@
+let
+NIXPKGS_env = builtins.getEnv "NIXPKGS";
+pkgsPath = if NIXPKGS_env == "" then <nixpkgs> else NIXPKGS_env;
+pkgs = import pkgsPath {};
+allOutputNames = packages: builtins.attrNames
+      (pkgs.lib.fold
+        (a: b: b //
+          (builtins.listToAttrs (map (x: {name = x; value = x;}) a.outputs or ["out"])))
+        {} packages);
+fullEnv = name: packages:
+  pkgs.buildEnv {
+      name = name;
+      paths = packages;
+      ignoreCollisions = false;
+      checkCollisionContents = true;
+      pathsToLink = ["/"];
+      extraOutputsToInstall = (allOutputNames packages);
+    };
+in with pkgs;
+
+linkFarm "raskin-heavy-packages" ([
+  { name = "main-heavy-package-set";
+    path = (fullEnv "main-heavy-package-set"
+      [
+        prosody matrix-synapse dovecot postfix
+        bind dhcp dnsmasq lighttpd nginx
+      ]);}
+])
