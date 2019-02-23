861bf76fcbebfd83b061b29be159180dcfbfc67e
Michael Raskin <7c6f434c@mail.ru>
2019-02-23 02:46:06 +0100

    Syncing from Monotone at 20190223-024605. Last commit message was More stuff

--- lesser-expressions/raskin-user-light.nix
+++ lesser-expressions/raskin-user-light.nix
@@ -23,7 +23,7 @@ fullEnv "main-light-package-set"
         squid git monotone fbida fbterm postgresql95 expect pmount fdm python2
         slmenu mcabber ii irssi elinks links2 rsync ratpoison xdummy
         pv lvm2 mariadb remind xterm zsh mlterm ntp mc ncdu ltrace weechat
-        htop iotop powertop mtr bind inotify-tools xorg.setxkbmap
+        htop iotop powertop mtr bind inotify-tools xorg.setxkbmap xorg.xev
         xfig transfig kig
         firefox vimHugeX evince xpdf zathura mplayer alsaUtils xvfb_run
         xorg.xmodmap bc xdotool lftp wget unzip p7zip gnumake xcape xorg.xrandr
