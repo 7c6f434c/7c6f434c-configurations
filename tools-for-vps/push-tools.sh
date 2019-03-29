ede91b6e4f1f630fd7773c34d39d371976445587
Michael Raskin <7c6f434c@mail.ru>
2019-03-29 02:46:47 +0100

    Syncing from Monotone at 20190329-024647. Last commit message was Some more additions

--- tools-for-vps/push-tools.sh
+++ tools-for-vps/push-tools.sh
@@ -17,10 +17,13 @@ ssh root@"$1" chmod a+rX -R /nix
 ssh root@"$1" ln -sfT "$toolset" ./tools
 ssh raskin@"$1" ln -sfT "$toolset" ./tools
 scp ii-starter ii-summarise raskin@"$1":
-ssh root@"$1" mkdir /etc/openvpn/
+ssh root@"$1" mkdir -p /etc/openvpn/
 scp openvpn.private/server*.conf "root@$1:/etc/openvpn/"
 scp openvpn-keys.private/pki/ca.crt  openvpn-keys.private/pki/private/server.key   openvpn-keys.private/pki/issued/server.crt openvpn-keys.private/2048.dhparam "root@$1:/etc/openvpn/"
 scp ii.service openvpn.service "root@$1:/lib/systemd/system/"
 scp nat "root@$1:/etc/network/if-up.d/nat"
 ssh root@"$1" systemctl enable ii
 ssh root@"$1" systemctl enable openvpn
+scp env "root@$1:/etc/env"
+ssh raskin@"$1" "sh -c 'grep /etc/env .bashrc || echo . /etc/env >> .bashrc'"
+ssh   root@"$1" "sh -c 'grep /etc/env .bashrc || echo . /etc/env >> .bashrc'"
--- tools-for-vps/vps-side.nix
+++ tools-for-vps/vps-side.nix
@@ -2,7 +2,7 @@ with import <nixpkgs> {};
 with rec {
         toolset = pkgs.buildEnv {
           name = "tools-for-vps";
-          paths = [ openvpn matrix-synapse ii screen nginx less rsync git monotone fossil mercurial vim strace transmission glibcLocales ];
+          paths = [ openvpn matrix-synapse ii screen nginx less rsync git monotone fossil mercurial vim strace transmission glibcLocales host dnsutils mtr htop iotop hping socat ];
         };
 };
 toolset
