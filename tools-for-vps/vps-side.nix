a51b359921475e149afa1fdbf943e9c98077a801
Michael Raskin <7c6f434c@mail.ru>
2019-04-01 01:38:54 +0200

    Syncing from Monotone at 20190401-013853. Last commit message was Fixes

--- tools-for-vps/vps-side.nix
+++ tools-for-vps/vps-side.nix
@@ -2,7 +2,11 @@ with import <nixpkgs> {};
 with rec {
         toolset = pkgs.buildEnv {
           name = "tools-for-vps";
-          paths = [ openvpn matrix-synapse ii screen nginx less rsync git monotone fossil mercurial vim strace transmission glibcLocales host dnsutils mtr htop iotop hping socat ];
+          paths = [ openvpn matrix-synapse ii screen nginx less
+                rsync git monotone fossil mercurial vim strace transmission
+                glibcLocales host dnsutils mtr htop iotop hping socat iftop
+                curl wget youtube-dl
+          ];
         };
 };
 toolset
