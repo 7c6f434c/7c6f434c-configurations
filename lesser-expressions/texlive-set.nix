07ef1f7d44b48b59ec023126075dbceef5f7b08b
Michael Raskin <7c6f434c@mail.ru>
2019-07-03 02:46:43 +0200

    Syncing from Monotone at 20190703-024643. Last commit message was More stuff

--- lesser-expressions/texlive-set.nix
+++ lesser-expressions/texlive-set.nix
@@ -27,6 +27,6 @@ pkgs.texlive.combine {
     xstring wrapfig ifoddpage inlinedef draftwatermark everypage
     enumitem cutwin moreverb mparhack bbding thmtools csquotes stmaryrd
     cleveref cancel ncctools changebar tocbibind shadow datetime fmtcount
-    bussproofs l3kernel cclicenses xifthen ifmtarg
+    bussproofs l3kernel cclicenses xifthen ifmtarg blindtext
     ;
 }
