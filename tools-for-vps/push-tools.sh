b9547e9e5c56498b9f0afd3146017444e358167b
Michael Raskin <7c6f434c@mail.ru>
2020-03-22 02:47:02 +0100

    Syncing from Monotone at 20200322-024702. Last commit message was Nginx SSL template

--- tools-for-vps/push-tools.sh
+++ tools-for-vps/push-tools.sh
@@ -7,14 +7,10 @@ test -n "$1" || {
 
 nix build -f ./vps-side.nix -o ~/.nix-personal/vps-tools
 toolset="$(readlink -f ~/.nix-personal/vps-tools)"
-nix copy "$toolset" --to ssh://root@"$1"
+nix copy "$toolset" -s --to ssh://root@"$1"
 
-ssh root@"$1" nix-store --indirect --add-root ./tools -r "$toolset"
+ssh root@"$1" ./tools/bin/nix-store --indirect --add-root ./tools -r "$toolset"
 ssh raskin@"$1" ln -sfT "$toolset" ./tools
 ssh matrix@"$1" ln -sfT "$toolset" ./tools
-ssh root@"$1" "bash -c 'cd ./tools/global/; find . -type d | while read i; do mkdir -vp /\$i; done; find . -type f | while read i; do ln -vsfT \$(readlink -f \$i) /\$i; done'"
+ssh root@"$1" ./tools/bin/remote-deploy
 scp openvpn-keys.private/pki/ca.crt  openvpn-keys.private/pki/private/server.key   openvpn-keys.private/pki/issued/server.crt openvpn-keys.private/2048.dhparam "root@$1:/etc/openvpn/"
-ssh root@"$1" "bash -c 'for i in ii openvpn openvpn-tcp; do systemctl enable \$i; done'"
-ssh raskin@"$1" "sh -c 'grep /etc/env .bashrc || echo . /etc/env >> .bashrc'"
-ssh   root@"$1" "sh -c 'grep /etc/env .bashrc || echo . /etc/env >> .bashrc'"
-ssh matrix@"$1" "sh -c 'grep /etc/env .bashrc || echo . /etc/env >> .bashrc'"
--- tools-for-vps/vps-side.nix
+++ tools-for-vps/vps-side.nix
@@ -16,6 +16,60 @@ with rec {
           ncp ${./openvpn-tcp.service} ./lib/systemd/system
           ncp ${./nat} ./etc/network/if-up.d
           ncp ${./env} ./etc
+          ncp ${./nginx.conf} ./var/nginx/conf
+          ncp ${./nginx.service} ./lib/systemd/system
+          ncp ${./dehydrated.conf} ./var/dehydrated
+          ncp ${./domains.txt.private} ./var/dehydrated
+          ncp ${./dehydrated.service} ./lib/systemd/system
+          ncp ${./dehydrated.timer} ./lib/systemd/system
+          ncp ${./postfix/main.cf} ./etc/postfix
+          ncp ${./postfix/master.cf} ./etc/postfix
+          ncp ${./postfix.service} ./lib/systemd/system
+
+          sed -e 's/@@@/'"$(cat ${./domains.txt.private} | xargs)"'/' -i ./etc/postfix/main.cf
+          for i in $(cat ${./domains.txt.private}); do
+                sed -e "s/@@@/$i/g" < ${./nginx.ssl.conf} >> ./var/nginx/conf/nginx.ssl.conf
+          done
+
+          mkdir -p ./var/log/nginx ./var/www ./var/www/.well-known ./var/nginx/logs \
+            ./var/dehydrated/conf.d ./var/dehydrated/acme-challenge \
+            ./var/lib/postfix/queue ./var/spool/mail ./var/mail ./var/spool/postfix
+        '';
+        remoteDeploy = pkgs.writeScriptBin "remote-deploy" ''
+          for i in root raskin matrix; do
+            su - $i sh -c 'grep /etc/env .bashrc || echo . /etc/env >> .bashrc'
+          done
+          (
+            cd ./tools/global;
+            find . -type d | while read i; do mkdir -vp /$i; done
+            find . -type f | while read i; do ln -vsfT $(readlink -f $i) /$i; done
+          )
+          for i in ii openvpn openvpn-tcp nginx dehydrated.timer postfix; do systemctl enable $i; done;
+          (
+            cd ./tools/global/lib/systemd/system
+            for i in *; do
+              systemctl reenable /lib/systemd/system/$i
+            done
+          )
+          for i in nginx; do systemctl restart $i; done
+
+          for u in www-data postfix; do
+            grep "^$u:" /etc/passwd || useradd -s /sbin/nologin -r $u
+          done
+          for g in postfix postdrop mail; do
+            grep "^$g:" /etc/group || groupadd -r $g
+          done
+
+          ln -sfT /var/dehydrated/acme-challenge /var/www/.well-known/acme-challenge
+
+          usermod -G mail postfix
+          chgrp mail /var/mail /var/spool/mail /var/spool/postfix /var/lib/postfix/queue
+          chmod g+ws /var/mail /var/spool/mail /var/spool/postfix
+          chown root /var/lib/postfix/queue
+          chmod u=rwx,og=rx /var/lib/postfix/queue
+
+          touch /etc/aliases
+          /root/tools/bin/newaliases
         '';
         toolset = pkgs.buildEnv {
           name = "tools-for-vps";
@@ -23,7 +77,8 @@ with rec {
                 rsync git monotone fossil mercurial vim strace transmission
                 glibcLocales host dnsutils mtr htop iotop hping socat iftop
                 curl wget youtube-dl jemalloc nix dehydrated netcat tcpdump
-                globalLinks
+                alpine postfix dovecot shared_mime_info
+                globalLinks remoteDeploy
           ];
         };
 };
