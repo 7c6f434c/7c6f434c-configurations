73d3943c50525a8d9016e8bd8d82ec514a70f48b
Michael Raskin <7c6f434c@mail.ru>
2020-05-03 02:46:39 +0200

    Syncing from Monotone at 20200503-024639. Last commit message was More stuff

--- lesser-expressions/raskin-user-heavy.nix
+++ lesser-expressions/raskin-user-heavy.nix
@@ -31,6 +31,6 @@ linkFarm "raskin-heavy-packages" ([
         xorg.xinput usbutils wgetpaste gdb scowl xcalib fmbt eprover glucose
         nginx cfdg highlight transmission spass iprover cvc4 z3 z3-tptp prover9
         signal-desktop mustache-spec mustache-go zxing
-        /* cachix */ darcs petrinizer
+        /* cachix */ darcs /* petrinizer */
       ]);}
 ])
