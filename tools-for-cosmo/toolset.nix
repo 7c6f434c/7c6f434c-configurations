c8bed420ba1ccd4887c0bb0153b927fdeac36ab8
Michael Raskin <7c6f434c@mail.ru>
2020-08-29 02:46:40 +0200

    Syncing from Monotone at 20200829-024640. Last commit message was Unlock botan platforms

---
+++ tools-for-cosmo/toolset.nix
@@ -0,0 +1,13 @@
+with import <nixpkgs> {};
+with rec {
+  toolset = pkgs.buildEnv {
+    name = "tools";
+    paths = [
+      nix
+      (monotone.override{
+        botan = (botan.override { openssl = null; }).overrideAttrs (x: { meta = null; });
+      })
+    ];
+  };
+};
+toolset
