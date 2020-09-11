7fe35d36308fcd9089413727aa31af35c9728521
Michael Raskin <7c6f434c@mail.ru>
2020-09-11 17:29:10 +0200

    Syncing from Monotone at 20200911-172910. Last commit message was Importing Matlab FHS env

---
+++ matlab-fhs-env.nix
@@ -0,0 +1,39 @@
+with import <nixpkgs> {};
+buildFHSUserEnv
+{
+  name = "matlab-env";
+  targetPkgs = p:
+  (with p;
+  [
+    mesa.osmesa aspell portaudio pixman harfbuzz libxml2 qt5.
+    qtbase libffi udev coreutils alsaLib dpkg gcc48 freetype glib fontconfig
+    openssl which ncurses jdk11 pam dbus_glib dbus pango gtk2-x11
+    atk gdk_pixbuf cairo ncurses5 mesa_glu zlib libglvnd
+  ])
+  ++ 
+  (with p.xorg;
+  [
+    libX11
+    libXcursor
+    libXrandr
+    libXext
+    libSM
+    libICE
+    libX11
+    libXrandr
+    libXdamage
+    libXrender
+    libXfixes
+    libXcomposite
+    libXcursor
+    libxcb
+    libXi
+    libXScrnSaver
+    libXtst
+    libXt
+    libXxf86vm
+    libXpm
+    libXp
+    libXmu
+  ]);
+}
