a4036aa03f8f7c46715e58d59bc6bdf47ce1c4d9
Michael Raskin <7c6f434c@mail.ru>
2021-02-25 02:47:13 +0100

    Syncing from Monotone at 20210225-024713. Last commit message was TLS/SNI for Postfix, and running Dovecot with TLS/SNI

--- tools-for-vps/vps-side.nix
+++ tools-for-vps/vps-side.nix
@@ -6,7 +6,9 @@ with rec {
         globalLinks = pkgs.runCommand "global-links" {} ''
           ncp () {
                 mkdir -p "$2"
-                cp "$1" "$2"/"$(basename "$1" | sed -re 's/^[a-z0-9]+-//')"
+                local tgt="$2"/"$(basename "$1" | sed -re 's/^[a-z0-9]+-//')"
+                cp "$1" "$tgt"
+                chmod u+w "$tgt"
           }
           mkdir -p "$out/global/"
           cd "$out/global/"
@@ -25,20 +27,32 @@ with rec {
           ncp ${./domains.txt.private} ./var/dehydrated
           ncp ${./dehydrated.service} ./lib/systemd/system
           ncp ${./dehydrated.timer} ./lib/systemd/system
+          ncp ${./postfix-key-concat.service} ./lib/systemd/system
+          ncp ${./postfix-key-concat.timer} ./lib/systemd/system
           ncp ${./postfix/main.cf} ./etc/postfix
           ncp ${./postfix/master.cf} ./etc/postfix
+          ncp ${./postfix.private/pointless-postfix.crt} ./etc/postfix
           ncp ${./postfix.service} ./lib/systemd/system
+          ncp ${./dovecot.conf} ./etc/dovecot
+          ncp ${./pam/dovecot} ./etc/pam.d
+          ncp ${./dovecot.service} ./lib/systemd/system
+          ncp ${./dovecot.private/pointless-dovecot.key} ./etc/dovecot
+          ncp ${./dovecot.private/pointless-dovecot.crt} ./etc/dovecot
           ncp ${fontsConf} ./etc/fonts
 
           sed -e 's/@@@/'"$(cat ${./domains.txt.private} | xargs)"'/' -i ./etc/postfix/main.cf
           for i in $(cat ${./domains.txt.private}); do
                 sed -e "s/@@@/$i/g" < ${./nginx.ssl.conf} >> ./var/nginx/conf/nginx.ssl.conf
+                sed -e "s/@@@/$i/g" < ${./dovecot.conf.per-domain} >> ./etc/dovecot/dovecot.conf
+                sed -e "s/@@@/$i/g" < ${./postfix/sni} >> ./etc/postfix/sni
           done
+          echo "mail_plugin_dir = ${dovecot}/lib/dovecot" >> ./etc/dovecot/dovecot.conf
 
           mkdir -p ./var/log/nginx ./var/www ./var/www/.well-known ./var/nginx/logs \
             ./var/dehydrated/conf.d ./var/dehydrated/acme-challenge \
             ./var/lib/postfix/queue ./var/spool/mail ./var/mail ./var/spool/postfix \
             ./var/cache/nginx
+          ln -sfT ${dovecot}/lib/dovecot ./etc/dovecot/modules
         '';
         remoteDeploy = pkgs.writeScriptBin "remote-deploy" ''
           for i in root raskin matrix; do
@@ -48,18 +62,9 @@ with rec {
             cd ./tools/global;
             find . -type d | while read i; do mkdir -vp /$i; done
             find . -type f | while read i; do ln -vsfT $(readlink -f $i) /$i; done
+            find . -type l | while read i; do ln -vsfT $(readlink -f $i) /$i; done
           )
-          for i in ii openvpn openvpn-tcp nginx dehydrated.timer postfix; do systemctl enable $i; done;
-          (
-            cd ./tools/global/lib/systemd/system
-            for i in *; do
-              rm -v /etc/systemd/$i /etc/systemd/*/$i
-              systemctl enable /lib/systemd/system/$i
-            done
-          )
-          for i in nginx; do systemctl restart $i; done
-
-          for u in www-data postfix; do
+          for u in www-data postfix dovenull dovecot; do
             grep "^$u:" /etc/passwd || useradd -s /sbin/nologin -r $u
           done
           for g in postfix postdrop mail; do
@@ -76,6 +81,20 @@ with rec {
 
           touch /etc/aliases
           /root/tools/bin/newaliases
+          ${postfix}/bin/postmap -c /etc/postfix -F /etc/postfix/sni
+
+          for i in nginx dovecot; do
+            systemctl restart $i;
+          done
+          for i in ii openvpn openvpn-tcp nginx dehydrated.timer postfix-key-concat.timer postfix dovecot; do systemctl enable $i; done;
+          (
+            cd ./tools/global/lib/systemd/system
+            for i in *; do
+              rm -v /etc/systemd/$i /etc/systemd/*/$i
+              systemctl enable /lib/systemd/system/$i
+            done
+          )
+
         '';
         toolset = pkgs.buildEnv {
           name = "tools-for-vps";
