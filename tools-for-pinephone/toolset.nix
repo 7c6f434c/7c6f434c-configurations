187aca74e3fcba90b61e85b1b104acade0b24304
Michael Raskin <7c6f434c@mail.ru>
2022-04-16 02:47:52 +0200

    Syncing from Monotone at 20220416-024752. Last commit message was

--- tools-for-pinephone/toolset.nix
+++ tools-for-pinephone/toolset.nix
@@ -13,6 +13,37 @@ with rec {
       dmtx-utils
       diffutils findutils
       vim_configurable
+      element-desktop
+      dict
+      (dictDBCollector {
+        dictlist = (with dictdDBs; map 
+        (x:{
+          name = x.dbName;
+          filename = x.outPath;
+          locale = x.locale;
+        })
+        [ 
+          eng2fra fra2eng eng2nld
+          nld2eng eng2rus
+          eng2deu deu2eng
+          mueller_enru_abbr
+          mueller_enru_base
+          mueller_enru_dict
+          mueller_enru_geo
+          mueller_enru_names
+        ]) ++ [
+          { 
+            name = "wiktionary-en";
+            filename = "${dictdDBs.wiktionary}/share/dictd/wiktionary-en";
+            locale = "en_US.UTF-8";
+            }
+            { 
+            name = "wordnet";
+            filename = "${dictdDBs.wordnet}/share/dictd/wn";
+            locale = "en_US.UTF-8";
+            }
+            ];
+            })
     ];
   };
 };
