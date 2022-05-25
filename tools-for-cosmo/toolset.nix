5be6c6f89efb66ee48743faa3f95328a90cc6f61
Michael Raskin <7c6f434c@mail.ru>
2022-05-25 02:46:30 +0200

    Syncing from Monotone at 20220525-024630. Last commit message was more stuff

--- tools-for-cosmo/toolset.nix
+++ tools-for-cosmo/toolset.nix
@@ -9,7 +9,12 @@ with rec {
       file remind
       pv wavemon nmap
       gcc11 m4 rsync gnumake
+      age fzf
       (import ../lesser-expressions/texlive-set.nix pkgs)
+    (runCommandCC "in-pty" {} ''
+      mkdir -p "$out/bin"
+      cc -lutil ${../lang-os/c/in-pty.c} -o "$out/bin/in-pty"
+    '')
     ];
   };
 };
