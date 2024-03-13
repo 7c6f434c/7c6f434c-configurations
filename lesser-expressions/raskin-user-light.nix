ca027f134db22decb4120c593d44ac7d477c0d53
Michael Raskin <7c6f434c@mail.ru>
2024-03-13 02:53:06 +0100

    Syncing from Monotone at 20240313-025306. Last commit message was drop old matrixcli

--- lesser-expressions/raskin-user-light.nix
+++ lesser-expressions/raskin-user-light.nix
@@ -67,23 +67,7 @@ fullEnv "main-light-package-set"
         squid git monotone fbida fbterm postgresql_13 expect pmount fdm
         fzf mcabber ii irssi links2 rsync ratpoison xdummy
         elinks
-        (matrixcli.overrideAttrs (x: {
-          postPatch = ''
-            sed -e '
-              s@!= client.user_id:@!= client.user_id or args.include_user:@;
-              /return parser/i\    parser_tail.add_argument("-i", "--include-user", dest="include_user", action="store_true", help="include own messages")
-              /return parser/i\    parser_listen.add_argument("-i", "--include-user", dest="include_user", action="store_true", help="include own messages")
-              s@\['"'"'url'"'"'\]@.get('"'"'url'"'"',"mxc://")@
-              '"s@\['msgtype']@.get('msgtype')@"'
-              s@listen_handler()@listen_handler(sys.exc_info()[0]); sender_name=event["sender"]; avatar_url="unknown://"; break@
-              /def listen_handler(e):/a\    try:
-              /def listen_handler(e):/a\      print(e)
-              /def listen_handler(e):/a\    except:
-              /def listen_handler(e):/a\      pass
-              s@content = download_url@content = (event\["content"].get("body") or ("{{ no event body, type: " + event\["type"] + " }}")) +"\\n" + (download_url or "no_url://")@
-            ' -i matrixcli
-          '';
-        }))
+        
         matrix-commander
         pv lvm2 mariadb remind xterm zsh 
         (mlterm.override {enableFeatures = mlterm.enableFeatures // {ssh2 = false;};})
